4fa41ec4dd9c8014ecd326855f07ba489819f807
unknown <zachariah11jerobert@gmail.com>
2022-03-20 10:59:21 +0530

    initial firbase setup

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const firebaseConfig = {\r\n    apiKey: \"AIzaSyB5SYuBG6Xc2rWN4mA3wyQzA8joEizF8Og\",\r\n    authDomain: \"netninja-fribase-9-dojo.firebaseapp.com\",\r\n    projectId: \"netninja-fribase-9-dojo\",\r\n    storageBucket: \"netninja-fribase-9-dojo.appspot.com\",\r\n    messagingSenderId: \"753546574582\",\r\n    appId: \"1:753546574582:web:6f780bbdf42a8778a80ad7\"\r\n  };\n\n//# sourceURL=webpack://firbase-9-dojo/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firbase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB5SYuBG6Xc2rWN4mA3wyQzA8joEizF8Og",
+  authDomain: "netninja-fribase-9-dojo.firebaseapp.com",
+  projectId: "netninja-fribase-9-dojo",
+  storageBucket: "netninja-fribase-9-dojo.appspot.com",
+  messagingSenderId: "753546574582",
+  appId: "1:753546574582:web:6f780bbdf42a8778a80ad7",
+};
+
+initializeApp(firebaseConfig);
